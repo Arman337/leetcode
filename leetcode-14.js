5e8e8c64c61fb0c0e9b820a756853a89208c2752
Arman337 <armanmovsesyan87@gmail.com>
2022-12-29 22:07:09 +0400

    done leetcode-14

--- leetcode-14.js
+++ leetcode-14.js
@@ -2,3 +2,20 @@
  Write a function to find the longest common prefix string amongst an array of strings.
  If there is no common prefix, return an empty string "".
 */
+
+function longestCommonPrefix(str) {
+  let strSort = str.sort();
+  let first = strSort[0];
+  let last = strSort[str.length - 1];
+  let result = "";
+
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] === last[i]) {
+      result += first[i];
+    } 
+    else if (first[i] !== last[i]) {
+      break;
+    }                                            
+  }
+  return result;
+} /*time complexity O(n * n) space complexity O(1)*/
